faeaecb8b5f2c9d160ef62e6b69ef78f12159ba4
angelicahg <herrerangelik@gmail.com>
2021-10-22 19:37:00 -0400

    cracion de HELPERS

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,15 +1,51 @@
 const { response } = require("express");
-
-
+const Usuario = require('../models/usuario.js')
+const bcrypt = require('bcryptjs');
 
 
 const crearUsuario = async (req, res = response) => {
-  res.json({
 
-    ok: true,
-    msg: 'new'
+  try {
+    const { email, password } = req.body;
 
-  });
+    //verificar que el email no exista
+    const existeEmail = await Usuario.findOne({ email });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo ya está registrado'
+      });
+    }
+
+
+    const usuario = new Usuario(req.body);
+    //eccriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+
+
+    // Guardar usuario en DB
+
+    await usuario.save();
+
+    //Generar el JWT 
+
+
+
+    res.json({
+      usuario
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    });
+  }
+
+
+
+
 
 }
 
